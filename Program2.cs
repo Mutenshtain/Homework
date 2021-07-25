5d6a62757dccfd9615ee19b3b33ca3d2c9d2e426
Алексей <87387584+Mutenshtain@users.noreply.github.com>
2021-07-25 19:36:35 +0300

    Add files via upload
    2 задача

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("число четное");
+            }
+            else
+            {
+                Console.WriteLine("число не четное");
+            }
+
+
+
+
+
+        }
+    }
+}
