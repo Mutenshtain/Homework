d24b822d246ddcd660b8aa7c100a2535d6f36b3a
Алексей <87387584+Mutenshtain@users.noreply.github.com>
2021-07-25 19:37:11 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSEN2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Maxtemp, MinTemp;
+
+            Console.WriteLine ("введите максимальную суточную температуру");
+
+            Maxtemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine ("введите минимальную суточную температуру");
+
+            MinTemp = double.Parse(Console.ReadLine());
+
+            double result = (Maxtemp + MinTemp) / 2;
+
+            Console.WriteLine("среднесуточая температура = " + result);
+            
+            Console.ReadKey();
+        }
+    }
+}
